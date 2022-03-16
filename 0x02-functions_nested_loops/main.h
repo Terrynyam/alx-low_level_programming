57fabc3f31a0d74a6b64c54f83cfa8bf8b11dbdc
Terrynyam <Terrynyamz93@gmail.com>
2022-03-16 16:02:56 +0200

    C functions and Nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,10 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
