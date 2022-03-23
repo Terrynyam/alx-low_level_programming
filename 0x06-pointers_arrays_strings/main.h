d6e513d2366dfddc3f1138c0904126304e0cb812
Terrynyam <terrynyamz93@gmail.com>
2022-03-23 23:39:26 +0000

    More pointers and Arrays

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -13,5 +14,5 @@ char *rot13(char *);
 void print_number(int n);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-#endif
 
+#endif
